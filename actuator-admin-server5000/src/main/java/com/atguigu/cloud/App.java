e97ea4ae2af5b07f1f761f672270e10bfa14885f
wxp <1900201075@qq.com>
2022-05-27 20:01:56 +0800

    123

--- actuator-admin-server5000/src/main/java/com/atguigu/cloud/App.java
+++ actuator-admin-server5000/src/main/java/com/atguigu/cloud/App.java
@@ -9,5 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
+        SpringApplication.run(App.class,args);
     }
 }
