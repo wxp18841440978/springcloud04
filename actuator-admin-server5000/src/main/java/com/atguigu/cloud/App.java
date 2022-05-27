b04bf889d91229c5a92ca67cc495b270bd15eef6
wxp <1900201075@qq.com>
2022-05-27 17:42:45 +0800

    123

---
+++ actuator-admin-server5000/src/main/java/com/atguigu/cloud/App.java
@@ -0,0 +1,13 @@
+package com.atguigu.cloud;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
